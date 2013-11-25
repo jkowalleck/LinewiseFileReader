53e0e54e283222fde751dc79c98c1ec695bbc521
Jan Kowalleck <jan.kowalleck@gfk.com>
2013-11-25 21:44:32 +0100

    * added event "onerror" * improved buffer size * moved fileReaderFunctions

--- LinewiseFileReader.js
+++ LinewiseFileReader.js
@@ -1,9 +1,9 @@
 /**
- * line-wise file reader 
+ * line-wise file reader
  *
  * @author jan Kowalleck <jan.kowalleck@googlemail.com>
  */
- 
+
 LinewiseFileReader = function ()
 {
 	this.reset();
@@ -12,40 +12,33 @@ LinewiseFileReader = function ()
 LinewiseFileReader.prototype = {
 	constructor : LinewiseFileReader ,
 
-	reset : function () 
+	reset : function ()
 	{
 		this.lines = [];
 	} ,
-	
+
 	lineSplitRE : /\r?\n/ ,
-	
+
+	bufferSize :  6241 *  (/* fileEncoding warrant > */ 6 * 7 * 32 ) , // get 6bit, 7bit, 8bit, 16bit and 32bit encodings read correctly
+
 	read : function (file)
 	{
-		if ( ! file ) 
-		{ 
+		if ( ! file )
+		{
 			throw new Error("noFile");
 		}
-		
+
 		var file_size = file.size;
 		if ( ! file_size )
 		{
 			throw new Error("fileEmpty");
-		}		
-	
-		var linewiseFileReader = this;
-		linewiseFileReader.reset();
-		
-		var reader = new FileReader(); 
-		
-		var chunkStart = 0;
-		var chunkSize = 1024 * 100; // 100 KB
-		var readChunk = function (start)
-		{
-			var blob = file.slice(start, start+chunkSize);
-			reader.readAsText(blob);
 		}
 
-		var getProgress = function (type) 
+		var linewiseFileReader = this;
+		linewiseFileReader.reset();
+
+		var chunkStart = 0;
+		var getProgress = function (type)
 		{
 			return new ProgressEvent(type, {
 				  lengthComputable : true
@@ -53,32 +46,58 @@ LinewiseFileReader.prototype = {
 				, total : file_size
 				});
 		};
-		
-		var lineSplitRE = this.lineSplitRE;
+
+		var reader = new FileReader();
+
+		var lineSplitRE = linewiseFileReader.lineSplitRE;
 		var lastChunkRest = "";
 		reader.onloadend = function (loadend)
 		{
-			var reader = this;
+			// trigger onerror, onabort OR onload
+			// and in the end: trigger onloadend
 
-			var chunkLines = (lastChunkRest + reader.result).split(lineSplitRE);
-			lastChunkRest = chunkLines.pop();
-			linewiseFileReader.lines = linewiseFileReader.lines.concat(chunkLines);
-
-			chunkStart += loadend.loaded;
-			var abort = ( linewiseFileReader.onload && linewiseFileReader.onload(getProgress("load"))===false );
-			if ( !abort && chunkStart < file_size  )
+			if ( this.error )
 			{
-				readChunk(chunkStart);
+				linewiseFileReader,error = this.error;
+				linewiseFileReader.onerror && linewiseFileReader.onerror(getProgress("error"));
 			}
 			else
 			{
-				linewiseFileReader.onloadend && linewiseFileReader.onloadend(getProgress("loadend"));
+				var reader = this;
+
+				var chunkLines = (lastChunkRest + reader.result).split(lineSplitRE);
+				chunkStart += loadend.loaded;
+
+				var notDone = ( chunkStart < file_size );
+
+				if ( notDone )
+				{
+					lastChunkRest = chunkLines.pop();
+				}
+				linewiseFileReader.lines = linewiseFileReader.lines.concat(chunkLines);
+
+				var abort = ( linewiseFileReader.onload && linewiseFileReader.onload(getProgress("load"))===false );
+				console.info(abort , notDone , chunkStart , file_size);
+				if ( !abort && notDone )
+				{
+					linewiseFileReader.readChunk(reader, file, chunkStart);
+					return;
+				}
 			}
+
+			linewiseFileReader.onloadend && linewiseFileReader.onloadend(getProgress("loadend"));
 		};
 
-		readChunk(chunkStart);
+		this.readChunk(reader, file, chunkStart);
 	} ,
 
+	readChunk : function (reader, file, start)
+	{
+		var blob = file.slice(start, start+this.bufferSize);
+		reader.readAsText(blob);
+	} ,
+
+	onerror : null ,
 	onload : null , // return false to abort loading process
 	onloadend : null
 };
